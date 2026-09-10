c6ccaf9c9dc364bd4cbd802795f4668fca5cce84
Lenovo-83BF <escherichia30636@qq.com>
2026-09-10 17:31:50 +0800

    Prepare v0.2.2 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aiworkmonitor.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
     }
 
     buildTypes {
